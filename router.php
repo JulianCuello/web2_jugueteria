09cdd0a75209fe7b24a62ae9d308afa89ba66b3f
Julian Cuello <julianc_69@hotmail.com>
2024-09-11 19:17:49 -0300

    color e imagen en la nav

--- router.php
+++ router.php
@@ -4,6 +4,8 @@ require_once "about.php";
 
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
+ 
+
 //**
 //    TABLA DE RUTEO
 //    Action        Funcion
--- templates/header.php
+++ templates/header.php
@@ -27,22 +27,23 @@ $fecha = new DateTime();
 
     <!-- main header -->
     <header>
-        <nav class="navbar navbar-expand-lg bg-light">
-            <div class="container-fluid">
-              <a class="navbar-brand" href="">Jugueteria</a>
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                  <li class="nav-item">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="list">
+            <img src="img/hugoJuguetesLogo.jpg" alt="logo" width="70px">
+        </a>
+        <a class="navbar-brand" href="#">Jugueteria</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="home">Noticias</a>
-                  </li>
-                  <li class="nav-item">
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="about">Nosotros</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-    </header>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
