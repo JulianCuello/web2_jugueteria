fae720273c93b942898dd21c9377eb286ada4a08
Julian Cuello <julianc_69@hotmail.com>
2024-09-16 10:47:30 -0300

    modifico estructura en db, y agrego consultas

--- db_jugueteria.sql
+++ db_jugueteria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-09-2024 a las 16:48:34
+-- Tiempo de generación: 16-09-2024 a las 15:43:53
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -24,48 +24,66 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `categoria`
---
-
-CREATE TABLE `categoria` (
-  `idCategoria` int(20) NOT NULL,
-  `material` varchar(45) NOT NULL,
-  `disponible` tinyint(4) NOT NULL,
-  `origen` varchar(20) NOT NULL,
-  `imgCategoria` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `juguete`
 --
 
 CREATE TABLE `juguete` (
-  `id_juguete` int(20) NOT NULL,
-  `nombreProducto` varchar(20) NOT NULL,
-  `precio` int(20) NOT NULL,
-  `codigo` int(20) NOT NULL,
-  `img` tinyint(45) NOT NULL,
-  `marca` varchar(45) NOT NULL,
-  `idCategoria` int(45) NOT NULL
+  `id_juguete` int(45) NOT NULL,
+  `nombreProducto` varchar(45) NOT NULL,
+  `precio` int(45) NOT NULL,
+  `material` varchar(45) NOT NULL,
+  `id_marca` int(45) NOT NULL,
+  `codigo` int(45) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `juguete`
+--
+
+INSERT INTO `juguete` (`id_juguete`, `nombreProducto`, `precio`, `material`, `id_marca`, `codigo`) VALUES
+(1, 'autos chiquitos', 4500, 'plastico', 1, 24141),
+(2, 'cocina', 10500, 'acero', 2, 95124),
+(3, 'dinosaurios', 7100, 'goma', 3, 451275),
+(4, 'mario', 6000, 'plastico', 4, 68541);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `marca`
+--
+
+CREATE TABLE `marca` (
+  `id_marca` int(45) NOT NULL,
+  `origen` varchar(45) NOT NULL,
+  `img` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `marca`
+--
+
+INSERT INTO `marca` (`id_marca`, `origen`, `img`) VALUES
+(1, 'china', 'img/autos'),
+(2, 'argentina', 'img/cocina'),
+(3, 'francia', 'img/dinosaurios'),
+(4, 'brasil', 'img/mario');
 
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `categoria`
---
-ALTER TABLE `categoria`
-  ADD PRIMARY KEY (`idCategoria`);
-
---
 -- Indices de la tabla `juguete`
 --
 ALTER TABLE `juguete`
-  ADD PRIMARY KEY (`id_juguete`);
+  ADD PRIMARY KEY (`id_juguete`),
+  ADD KEY `fk_marca` (`id_marca`);
+
+--
+-- Indices de la tabla `marca`
+--
+ALTER TABLE `marca`
+  ADD PRIMARY KEY (`id_marca`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -75,17 +93,23 @@ ALTER TABLE `juguete`
 -- AUTO_INCREMENT de la tabla `juguete`
 --
 ALTER TABLE `juguete`
-  MODIFY `id_juguete` int(20) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_juguete` int(45) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `marca`
+--
+ALTER TABLE `marca`
+  MODIFY `id_marca` int(45) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `categoria`
+-- Filtros para la tabla `juguete`
 --
-ALTER TABLE `categoria`
-  ADD CONSTRAINT `categoria_ibfk_1` FOREIGN KEY (`idCategoria`) REFERENCES `juguete` (`id_juguete`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `juguete`
+  ADD CONSTRAINT `fk_marca` FOREIGN KEY (`id_marca`) REFERENCES `marca` (`id_marca`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
